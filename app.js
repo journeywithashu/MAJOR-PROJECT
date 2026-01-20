97b967a207bd87df99310f5c0b1e37e4153c3e89
Ashutosh Yadav <144024653+journeywithashu@users.noreply.github.com>
2026-01-20 07:52:15 +0530

    create new & Create Route GET /listings/new --->form -->submit

--- app.js
+++ app.js
@@ -35,6 +35,10 @@ app.get("/listings",async(req,res)=>{
      res.render("listings/index.ejs",{allListings});
 });
 
+//New Route
+app.get("/listings/new",(req,res)=>{
+     res.render("listings/new.ejs");
+});
 
 //Show Route
 app.get("/listings/:id",async(req,res)=>{
@@ -42,6 +46,19 @@ app.get("/listings/:id",async(req,res)=>{
      const listing = await Listing.findById(id);
      res.render("listings/show.ejs", { listing });
 });
+
+//Create Route
+app.post("/listings",async(req,res)=>{
+    //  let{title, description,image, country, location} = req.body;
+    //let listing = req.body;
+   const newListing = new Listing(req.body.listing);
+  await newListing.save();
+  res.redirect("/listings");
+    console.log(newListing);
+    
+})
+
+
 
 // app.get("/testlisting",async(req,res)=>{
 //      let sampleListing = new Listing({
