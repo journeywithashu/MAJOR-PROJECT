3756d77ccd02321ac0be4ff04e3a2b819ec8a6f9
Ashutosh Yadav <144024653+journeywithashu@users.noreply.github.com>
2026-01-29 13:08:41 +0530

    schema validation joi npm package install

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const methodOverride = require("method-override")
 const ejsMate = require("ejs-mate");
 const wrapAsync = require("./utils/wrapAsyc.js");
 const ExpressError = require("./utils/ExpressError.js");
+const { listingSchema } = require("./schema.js");
 
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
@@ -65,8 +66,12 @@ app.get("/listings/:id",async(req,res)=>{
 //Create Route
 app.post("/listings",
      wrapAsync(async(req,res,next)=>{
-          if(!req.body.listing) throw new ExpressError(400,"Invalid Listing Data");
-  const newListing = new Listing(req.body.listing);
+         let result = listingSchema.validate(req.body);
+         console.log(result);
+         if(result.error){
+          throw new ExpressError(400,result.error);
+         }
+         
   await newListing.save();
   res.redirect("/listings");
 
