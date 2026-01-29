c868bfa99ca605316596d0f64cfa4c2cfaf00a93
Ashutosh Yadav <144024653+journeywithashu@users.noreply.github.com>
2026-01-29 12:33:36 +0530

    error.js

--- app.js
+++ app.js
@@ -121,7 +121,8 @@ app.use((req,res,next)=>{
 
 app.use((err,req,res,next)=>{
      let {statusCode = 500, message="something went wrong"} = err;
-     res.status(statusCode).send(message);
+     //res.status(statusCode).send(message);
+     res.status(statusCode).render("error.ejs",{message,err});
 });
 
 app.listen(8080,()=>{
