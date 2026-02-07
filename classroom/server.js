151b94fdf6415fba13d36ed5dc2ce3b9527305e2
Ashutosh Yadav <144024653+journeywithashu@users.noreply.github.com>
2026-02-07 09:44:38 +0530

    Storing & Using Session Info

--- classroom/server.js
+++ classroom/server.js
@@ -2,20 +2,37 @@ const express = require('express');
 const app = express();
 const session = require('express-session');
 
-app.use(session({ secret:"mysecret",
+const sessionOptions = { 
+secret:"mysecret",
 resave: false,
 saveUninitialized: true
-}));
+};
+
+app.use(session(sessionOptions));
+
+app.get('/register', (req, res) => {
+     let {name ="annonymus"} = req.query;
+     req.session.name = name;
+     console.log(req.session.name);
+     
+     res.send(name);
+});
+
+app.get("/hello",(req,res)=>{
+     res.send(`hello,${req.session.name}`);
+});
 
 
-app.get("/reqcount",(req,res)=>{
-     if(req.session.count){
-          req.session.count++;
-     }else{
-     req.session.count = 1;
-     }
-     res.send(`you sent a request ${req.session.count} times`);
-})
+
+
+// app.get("/reqcount",(req,res)=>{
+//      if(req.session.count){
+//           req.session.count++;
+//      }else{
+//      req.session.count = 1;
+//      }
+//      res.send(`you sent a request ${req.session.count} times`);
+// })
      // app.get('/', (req, res) => {
      //      res.send("test successful");
      // });
