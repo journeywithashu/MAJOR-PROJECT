4035ce6d0f63de6bdcffb0b0d89609736b8c8c97
Ashutosh Yadav <144024653+journeywithashu@users.noreply.github.com>
2026-02-07 09:25:40 +0530

    Exploring Session Option

--- classroom/server.js
+++ classroom/server.js
@@ -2,11 +2,23 @@ const express = require('express');
 const app = express();
 const session = require('express-session');
 
-app.use(session({ secret:"mysecret"}));
+app.use(session({ secret:"mysecret",
+resave: false,
+saveUninitialized: true
+}));
 
-     app.get('/', (req, res) => {
-          res.send("test successful");
-     });
+
+app.get("/reqcount",(req,res)=>{
+     if(req.session.count){
+          req.session.count++;
+     }else{
+     req.session.count = 1;
+     }
+     res.send(`you sent a request ${req.session.count} times`);
+})
+     // app.get('/', (req, res) => {
+     //      res.send("test successful");
+     // });
 
 
 
