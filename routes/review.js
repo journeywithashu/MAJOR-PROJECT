e777bc37b1adbda0dc37b909a8c966967ed4c56d
Ashutosh Yadav <144024653+journeywithashu@users.noreply.github.com>
2026-02-12 01:50:40 +0530

    Setting Authorization

--- routes/review.js
+++ routes/review.js
@@ -4,16 +4,23 @@ const wrapAsync = require("../utils/wrapAsyc.js");
 const ExpressError = require("../utils/ExpressError.js");
 const Reviews = require("../models/review.js");
 const Listing = require("../models/listing.js");
-const{validateReview} = require("../middleware.js");
+const{validateReview,isLoggedIn} = require("../middleware.js");
 
 
 
 //Post review route
-router.post("/",validateReview,wrapAsync(async(req,res)=>{
+router.post(
+     "/",
+     isLoggedIn,
+     validateReview,
+     wrapAsync(async(req,res)=>{
      let listing = await Listing.findById(req.params.id);
      let newReview = new Reviews(req.body.review);
-
+     newReview.author = req.user._id;
+     console.log(newReview);
+     
      listing.reviews.push(newReview);
+
      await newReview.save();
      await listing.save();
      req.flash("success","Successfully added a new review!");
