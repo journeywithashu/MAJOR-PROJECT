76abcd5b5f96a727f315b39d16533dfaa66e58b4
Ashutosh Yadav <144024653+journeywithashu@users.noreply.github.com>
2026-02-12 00:01:57 +0530

    Listing Owner

--- init/index.js
+++ init/index.js
@@ -17,6 +17,9 @@ async function main(){
 
 const initDB = async () => {
     await Listing.deleteMany({});
+    initData.data = initData.data.map((obj)=>({
+        ...obj,
+         owner: '698ca2f2666d21c953d41087'}));
     await Listing.insertMany(initData.data);
     console.log("data was initialize");
     
--- routes/listing.js
+++ routes/listing.js
@@ -35,11 +35,13 @@ router.get("/new",isLoggedIn,(req,res)=>{
 //Show Route
 router.get("/:id",wrapAsync(async(req,res)=>{
      let {id} = req.params;
-     const listing = await Listing.findById(id).populate("reviews");
+     const listing = await Listing.findById(id).populate("reviews").populate("owner");
      if(!listing){
           req.flash("error","Cannot find the listing!");
           return res.redirect("/listings");
      }
+     console.log(listing);
+     
      res.render("listings/show.ejs", { listing });
 })
 );
@@ -50,6 +52,7 @@ router.post( "/",
      validateListing,
      wrapAsync(async(req,res)=>{
      const newListing = new Listing(req.body.listing);
+     newListing.owner = req.user._id;
      await newListing.save();
      req.flash("success","Successfully made a new listing!");
      res.redirect("/listings");
