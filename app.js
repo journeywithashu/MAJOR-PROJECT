6df23c726fee0ca7ebfba68e9aa0c10e8c505402
Ashutosh Yadav <144024653+journeywithashu@users.noreply.github.com>
2026-02-12 11:45:36 +0530

    Cloud setup

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
+if(process.env.NODE_ENV !== "production"){
+     require("dotenv").config();
+}
+console.log(process.env.SECRET);
+
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
@@ -10,6 +15,8 @@ const flash = require("connect-flash");
 const passport = require("passport");
 const LocalStrategy = require("passport-local");
 const User = require("./models/user.js");
+
+
 
 
 
